e1058caa6bd06f3b51dc3b19f9317383b45d5f8c
theo-githu <111968864+theo-githu@users.noreply.github.com>
2023-06-22 21:46:26 +0100

    Create navigation-bar.jsx

---
+++ src/components/navigation-bar/navigation-bar.jsx
@@ -0,0 +1,44 @@
+
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
+
+export const NavigationBar = ({ user, onLoggedOut }) => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark" expand="lg" fixed="top">
+        <Container>
+          <Navbar.Brand as={Link} to="/">
+            myFlix
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                {!user && (
+                  <>
+                    <Nav.Link as={Link} to="/login">
+                      Login
+                    </Nav.Link>
+                    <Nav.Link as={Link} to="/signup">
+                      Register
+                    </Nav.Link>
+                  </>
+                )}
+                {user && (
+                  <>
+                    <Nav.Link as={Link} to="/">
+                      Home
+                    </Nav.Link> 
+                    <Nav.Link as={Link} to={`/users/${user.Username}`}>
+                      myProfile
+                    </Nav.Link>
+                    <Nav.Link onClick={onLoggedOut}>Logout</Nav.Link>
+                  </>
+                )}
+              </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
